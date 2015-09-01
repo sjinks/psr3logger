9338dd5a83c7b707b281596520e231c54f9e2393
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2015-09-01 02:59:18 +0000

    WIP: changes for PHP 7

--- ext/psr/log/abstractlogger.c
+++ ext/psr/log/abstractlogger.c
@@ -6,10 +6,19 @@ zend_class_entry *psr_log_abstractlogger_ce;
 
 static void psr_log_abstractlogger_log_helper(INTERNAL_FUNCTION_PARAMETERS, const char *lvl)
 {
+#if PHP_MAJOR_VERSION >= 7
+	zval z_context;
+	zval z_level;
+#endif
 	zval *level, *message, *context = NULL;
-	zval *retval = NULL;
+#if PHP_MAJOR_VERSION >= 7
+	zval params[3];
+	zval retval;
+#else
 	zval **params[3] = { &level, &message, &context };
+	zval *retval = NULL;
 	zval function_name = zval_used_for_init;
+#endif
 	zend_fcall_info fci;
 	zend_fcall_info_cache fcic;
 	zend_class_entry *ce;
@@ -21,40 +30,76 @@ static void psr_log_abstractlogger_log_helper(INTERNAL_FUNCTION_PARAMETERS, cons
 	ce = Z_OBJCE_P(getThis());
 
 	if (!context) {
+#if PHP_MAJOR_VERSION >= 7
+		context = &z_context;
+#else
 		MAKE_STD_ZVAL(context);
+#endif
 		array_init(context);
 		Z_SET_REFCOUNT_P(context, 0);
 	}
 
+#if PHP_MAJOR_VERSION >= 7
+	level = &z_level;
+	ZVAL_STRING(level, lvl);
+#else
 	MAKE_STD_ZVAL(level);
 	ZVAL_STRING(level, lvl, 1);
+#endif
 	Z_SET_REFCOUNT_P(level, 0);
+
+#if PHP_MAJOR_VERSION >= 7
+	params[0] = z_level;
+	params[1] = *message;
+	params[2] = z_context;
+#endif
 
 	fci.size           = sizeof(fci);
 	fci.function_table = &ce->function_table;
-	fci.object_ptr     = getThis();
+#if PHP_MAJOR_VERSION >= 7
+	ZVAL_UNDEF(&fci.function_name);
+	fci.object         = Z_OBJ_P(getThis());
+#else
 	fci.function_name  = &function_name;
+	fci.object_ptr     = getThis();
 	fci.retval_ptr_ptr = &retval;
+#endif
 	fci.param_count    = 3;
 	fci.params         = params;
 	fci.no_separation  = 1;
 	fci.symbol_table   = NULL;
 
 	fcic.initialized   = 1;
+#if PHP_MAJOR_VERSION >= 7
+	fcic.object        = Z_OBJ_P(getThis());
+#else
 	fcic.object_ptr    = getThis();
+#endif
 	fcic.calling_scope = ce;
 	fcic.called_scope  = ce;
 
-	if (zend_hash_quick_find(fci.function_table, ZEND_STRS("log"), zend_inline_hash_func(ZEND_STRS("log")), (void**) &fcic.function_handler) == FAILURE) {
+#if PHP_MAJOR_VERSION >= 7
+	fcic.function_handler = zend_hash_str_find_ptr(fci.function_table, "log", sizeof("log")-1);
+	if (!fcic.function_handler) {
+		fcic.initialized = 0;
+		ZVAL_STRINGL(&fci.function_name, "log", sizeof("log")-1);
+	}
+#else
+	if (zend_hash_quick_find(fci.function_table, ZEND_STRS("log"), zend_inline_hash_func(ZEND_STRS("log")), (void**)&fcic.function_handler) == FAILURE) {
 		fcic.initialized = 0;
 		ZVAL_STRING(&function_name, "log", 0);
 	}
+#endif
 
 	zend_call_function(&fci, &fcic TSRMLS_CC);
 
 	if (retval) {
 		zval_ptr_dtor(&retval);
 	}
+
+#if PHP_MAJOR_VERSION >= 7
+	zval_ptr_dtor(&fci.function_name);
+#endif
 }
 
 PHP_FUNCTION(Psr_Log_Emergency)
