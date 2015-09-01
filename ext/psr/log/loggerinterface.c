a5b31de6837bc8c942c9b1b94fbff3d08edc19f6
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2015-09-01 14:54:24 +0000

    WIP: changes for PHP 7

--- ext/psr/log/loggerinterface.c
+++ ext/psr/log/loggerinterface.c
@@ -3,6 +3,7 @@
 zend_class_entry* psr_log_loggerinterface_ce;
 
 static const zend_function_entry fe_psr_log_loggerinterface[] = {
+/*
 	PHP_ABSTRACT_ME(Psr_Log_LoggerInterface, emergency, arginfo_psr_log_loggerinterface_logspecialized)
 	PHP_ABSTRACT_ME(Psr_Log_LoggerInterface, alert, arginfo_psr_log_loggerinterface_logspecialized)
 	PHP_ABSTRACT_ME(Psr_Log_LoggerInterface, critical, arginfo_psr_log_loggerinterface_logspecialized)
@@ -11,6 +12,7 @@ static const zend_function_entry fe_psr_log_loggerinterface[] = {
 	PHP_ABSTRACT_ME(Psr_Log_LoggerInterface, notice, arginfo_psr_log_loggerinterface_logspecialized)
 	PHP_ABSTRACT_ME(Psr_Log_LoggerInterface, info, arginfo_psr_log_loggerinterface_logspecialized)
 	PHP_ABSTRACT_ME(Psr_Log_LoggerInterface, debug, arginfo_psr_log_loggerinterface_logspecialized)
+*/
 	PHP_ABSTRACT_ME(Psr_Log_LoggerInterface, log, arginfo_psr_log_loggerinterface_log)
 	PHP_FE_END
 };
