1f658d029d95ee9d9a9bb986a96939643e04ed86
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2015-09-01 14:39:58 +0000

    WIP: changes for PHP 7

--- ext/psr/log/loglevel.c
+++ ext/psr/log/loglevel.c
@@ -9,14 +9,14 @@ int init_Psr_Log_LogLevel(TSRMLS_D)
 
 	psr_log_loglevel_ce = zend_register_internal_class(&ce TSRMLS_CC);
 	if (EXPECTED(psr_log_loglevel_ce != NULL)) {
-		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRS("EMERGENCY"), ZEND_STRS(PSR_LOG_LOGLEVEL_EMERGENCY) TSRMLS_CC);
-		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRS("ALERT"),     ZEND_STRS(PSR_LOG_LOGLEVEL_ALERT)     TSRMLS_CC);
-		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRS("CRITICAL"),  ZEND_STRS(PSR_LOG_LOGLEVEL_CRITICAL)  TSRMLS_CC);
-		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRS("ERROR"),     ZEND_STRS(PSR_LOG_LOGLEVEL_ERROR)     TSRMLS_CC);
-		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRS("WARNING"),   ZEND_STRS(PSR_LOG_LOGLEVEL_WARNING)   TSRMLS_CC);
-		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRS("NOTICE"),    ZEND_STRS(PSR_LOG_LOGLEVEL_NOTICE)    TSRMLS_CC);
-		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRS("INFO"),      ZEND_STRS(PSR_LOG_LOGLEVEL_INFO)      TSRMLS_CC);
-		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRS("DEBUG"),     ZEND_STRS(PSR_LOG_LOGLEVEL_DEBUG)     TSRMLS_CC);
+		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRL("EMERGENCY"), ZEND_STRL(PSR_LOG_LOGLEVEL_EMERGENCY) TSRMLS_CC);
+		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRL("ALERT"),     ZEND_STRL(PSR_LOG_LOGLEVEL_ALERT)     TSRMLS_CC);
+		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRL("CRITICAL"),  ZEND_STRL(PSR_LOG_LOGLEVEL_CRITICAL)  TSRMLS_CC);
+		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRL("ERROR"),     ZEND_STRL(PSR_LOG_LOGLEVEL_ERROR)     TSRMLS_CC);
+		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRL("WARNING"),   ZEND_STRL(PSR_LOG_LOGLEVEL_WARNING)   TSRMLS_CC);
+		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRL("NOTICE"),    ZEND_STRL(PSR_LOG_LOGLEVEL_NOTICE)    TSRMLS_CC);
+		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRL("INFO"),      ZEND_STRL(PSR_LOG_LOGLEVEL_INFO)      TSRMLS_CC);
+		zend_declare_class_constant_stringl(psr_log_loglevel_ce, ZEND_STRL("DEBUG"),     ZEND_STRL(PSR_LOG_LOGLEVEL_DEBUG)     TSRMLS_CC);
 
 		return SUCCESS;
 	}
